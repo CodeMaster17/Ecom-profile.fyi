7325a6dac27fdd2515576794fc3958f7bae995fd
Harshit Yadav <2105891@kiit.ac.in>
2024-08-13 00:57:25 +0530

    feat: created schema for product, brand and category

---
+++ backend/models/brand_schema.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+
+const brandSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Brand', brandSchema);
---
+++ backend/models/product_schema.js
@@ -0,0 +1,71 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    ratings: {
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    reviews: [{
+        user: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            required: true
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    discount: {
+        type: Number,
+        min: 0,
+        max: 100
+    },
+    specifications: {
+        type: Map,
+        of: String
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
