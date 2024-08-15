c1f33ba456f4dfe28fea0d5fe13ffe6f1362164a
Harshit Yadav <2105891@kiit.ac.in>
2024-08-15 12:26:59 +0530

    feat: brand schema updated

--- backend/models/brand_schema.js
+++ backend/models/brand_schema.js
@@ -1,12 +1,18 @@
-
 const mongoose = require('mongoose');
 
 const brandSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     description: {
+        type: String
+    },
+    logo: {
+        type: String
+    },
+    website: {
         type: String
     }
 });
